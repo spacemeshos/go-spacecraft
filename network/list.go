71c213eee4bc499c81e09a56cc55ff7573966284
Narayan Prusty <arsenal.narayan@gmail.com>
2022-01-05 17:02:53 +0530

    updated log lines

--- network/list.go
+++ network/list.go
@@ -1,9 +1,8 @@
 package network
 
 import (
-	"fmt"
-
 	"github.com/spacemeshos/go-spacecraft/gcp"
+	"github.com/spacemeshos/go-spacecraft/log"
 )
 
 func ListNetworks() error {
@@ -14,11 +13,11 @@ func ListNetworks() error {
 	}
 
 	if len(networks) == 0 {
-		fmt.Println("No networks found")
+		log.Error.Println("No networks found")
 	} else {
-		fmt.Println("Here is the list of deployed networks:")
+		log.Info.Println("Here is the list of deployed networks:")
 		for _, name := range networks {
-			fmt.Println(name)
+			log.Success.Println(name)
 		}
 	}
 
